f5a6d18134e43528a24596ee549f6d25645eaf28
kaana0 <k.swechocc@gmail.com>
2023-05-18 11:52:49 +0900

    firstCommit

---
+++ MauiApp1/NextPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿namespace MauiApp1;
+
+public partial class NextPage : ContentPage
+{
+	int count = 0;
+
+	public NextPage()
+	{
+		InitializeComponent();
+	}
+	private async void OnBackClicked(object sender, EventArgs e)
+	{
+	
+	}
+}
+
